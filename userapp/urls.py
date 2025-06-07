65acbf7b3b78db67d9627d2993fb4fd86392dee3
Mohaddeseh Akbari <78makbari@gmail.com>
2025-06-07 22:18:26 +0330

    add register form and model

--- userapp/urls.py
+++ userapp/urls.py
@@ -15,25 +15,6 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.urls import path
-from .views import register_view, login_view, logout_view, profile_view, edit_profile_view, CustomPasswordChangeView
-from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path('register/', register_view, name='register'),
-    path('login/', login_view, name='login'),
-    path('logout/', logout_view, name='logout'),
-    path('profile/', profile_view, name='profile'),
-    path('profile/edit/', edit_profile_view, name='edit_profile'),
-    path('change-password/', CustomPasswordChangeView.as_view(), name='change_password'),
-        # صفحه وارد کردن ایمیل
-    path('password-reset/', auth_views.PasswordResetView.as_view(template_name='accounts/password_reset.html'), name='password_reset'),
-
-    # صفحه‌ای که می‌گه ایمیل ارسال شد
-    path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='accounts/password_reset_done.html'), name='password_reset_done'),
-
-    # صفحه تعیین رمز جدید (لینک داخل ایمیل کاربر)
-    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='accounts/password_reset_confirm.html'), name='password_reset_confirm'),
-
-    # صفحه‌ی موفقیت
-    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='accounts/password_reset_complete.html'), name='password_reset_complete'),
 ]
